ca430e6f31e1cdf26c839b02af87bff31206e107
“lotheren” <lotheren@gmail.com>
2015-04-07 08:04:48 -0700

    adding a comment for branch dev2 now for push

--- Main.java
+++ Main.java
@@ -8,6 +8,7 @@ public class Main {
         //here I am adding new stuff to Dev2 to see the effects
         // I am adding a second set of notes for commit 2
         // last comment for the push ///
+        // test 1
         //some stuff here
     }
 }
