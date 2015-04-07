876737c2f19090094ba7f9d5f571e69210ab35fa
“lotheren” <lotheren@gmail.com>
2015-04-07 08:00:12 -0700

    adding a comment for branch dev2

--- Main.java
+++ Main.java
@@ -6,6 +6,7 @@ public class Main {
         String aString = aBook.a;
         System.out.println(aString); // Prints out whatever is in object A
         //here I am adding new stuff to Dev2 to see the effects
+        // I am adding a second set of notes for commit 2
         //some stuff here
     }
 }
