ed8e3f19b89e26b3ae79bd981371a420f9bb0861
“lotheren” <lotheren@gmail.com>
2015-04-07 07:54:12 -0700

    Added a System.out

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ public class Main {
         System.out.println("Hello World!");
         Book aBook = new Book();
         String aString = aBook.a;
-        System.out.println(aString);
+        System.out.println(aString); // Prints out whatever is in object A
         //some stuff here
     }
 }
